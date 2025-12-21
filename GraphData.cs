00f92467ee82951d6f81be13c1d3454deae70122
Gökalp Mümtaz Eroğlu <ggme756@gmail.com>
2025-12-21 22:35:53 +0300

    Edge ve GraphData yapısında yol/şerit desteği eklendi
    Edge yapısına ToNodeId, RoadId ve LaneId alanları eklendi, yeni kurucu tanımlandı. JunctionInfo sınıfı eklendi. GraphData'da NodeCoordinates artık Dictionary<string, Point> olarak tutuluyor, eski Point[] ise NodeCoordinatesArray olarak ayrıldı. AdjacencyList ve NodeIdToIndexMap property olarak güncellendi. Kod, yol ve şerit bazında daha esnek hale getirildi ve string düğüm ID desteği sağlandı.

--- GraphData.cs
+++ GraphData.cs
@@ -1,6 +1,7 @@
-﻿using System.Collections.Generic;
-
+﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+
 
 namespace ConsoleApp3
 {
@@ -10,50 +11,50 @@ namespace ConsoleApp3
     public struct Edge
     {
         public int ToNodeIndex { get; }
+        public string ToNodeId { get; set; }
         public double Weight { get; }
-
+        // Yeni: Yol ID - Eger bu kenar bir yola (road/way) aitse
+        public string RoadId { get; set; }
+        // Yeni: Lane ID
+        public int LaneId { get; set; }
         public Edge(int toNodeIndex, double weight)
         {
             ToNodeIndex = toNodeIndex;
             Weight = weight;
         }
+        public Edge(string toNodeId, double weight)
+        {
+            ToNodeId = toNodeId;
+            Weight = weight;
+            ToNodeIndex = -1;
+        }
+    }
+    public class JunctionInfo
+    {
+        public string Id { get; set; }
+        // Gelen Road ID -> List of bağlanan Road IDs
+        public Dictionary<string, List<string>> Connections { get; set; } = new Dictionary<string, List<string>>();
     }
 
-    
     /// Ayristirilmis harita verilerinden olusturulan grafi temsil eder.
     /// Adjacency List kullanarak bellek verimli calisir.
-    
+
     public class GraphData
     {
-        
+        public int NodeCount { get; private set; }
+        public int EdgeCount { get; private set; }
         /// Komsuluk listesi - buyuk graflar icin verimli
         /// Key: Dugum indeksi, Value: Bu dugumden cikan kenarlarin listesi
-        
-        public Dictionary<int, List<Edge>> AdjacencyList { get; }
 
-        
-        /// Dugum koordinatlari
-        
-        public Point[] NodeCoordinates { get; }
-       
-        
-        //Graf'taki toplam dugum sayisi
-        
-        public int NodeCount => NodeCoordinates?.Length ?? 0;
-
-        
+        public Dictionary<int, List<Edge>> AdjacencyList { get; private set; }
+        /// Dugum koordinatlari  
+        public Dictionary<string, Point>> Point[] NodeCoordinates { get; private set; }
+        public Point[] NodeCoordinatesArray { get; private set; }
         //Orijinal dosyadaki ID'leri (orn: OSM node ID, XODR junction ID) dizi indeksine esler.
-        
-        public Dictionary<string, int> NodeIdToIndexMap { get; }
-
-        
-        // Graf'taki toplam kenar sayisi
-        
-        public int EdgeCount { get; }
-
-        
+        public Dictionary<string, int> NodeIdToIndexMap { get; private set; }
+        // Graf'taki toplam kenar sayisi   
+        public int EdgeCount { get; } 
         /// Adjacency List ile GraphData olusturur (bellek verimli)
-        
         public GraphData(Dictionary<int, List<Edge>> adjacencyList, Point[] nodeCoordinates, Dictionary<string, int> nodeIdToIndexMap)
         {
             AdjacencyList = adjacencyList;
